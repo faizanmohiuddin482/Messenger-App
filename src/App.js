9334b608ca6f5f03e1dee2b9c4a9f17b291f536d
Mohammed Faizan Mohiuddin <faizanmohiuddin482@gmail.com>
2020-10-09 21:14:04 +0530

    added component and useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React,{useState} from 'react';
-
+import React,{useState,useEffect} from 'react';
+import { Button,FormControl,Input,InputLabel } from '@material-ui/core';
 import './App.css';
+import Message from './Message'
 
 function App() {
   //Initialise the state, example of react hook
   const [input,setInput] = useState('');
   const [messages,setMessages] = useState(['hello']);
-  console.log(input);
-console.log(messages);
+  const [username,setUsername] = useState('');
+ 
+  // useState=variable in REACT
+  // useEffect = run code on a condition in REACT
+  useEffect(()=>{
+      // const name = promt("Please enter your name");
+      setUsername(prompt('Please enter your name'));
+  },[])//condition
+
+
   const sendMessage = (event)=>{
+    event.preventDefault();
     //all the logic to send a message goes here
     setMessages([...messages,input])
     setInput('');
@@ -17,11 +27,17 @@ console.log(messages);
   return (
     <div className="App">
       <h1>Messenger</h1>
-      <input value={input} onChange={event => setInput(event.target.value)}/>
-      <button onClick={sendMessage}>Send Message</button>
-
+  <h2>Welcome {username}</h2>
+    <form>
+      <FormControl>
+        <InputLabel >Enter Message</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)} />
+          <Button disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage} disableElevation>Send Message</Button>
+       </FormControl>
+      
+    </form>
           {messages.map(message=>(
-            <p>{message}</p>
+            <Message text={message}/>
           ))}
 
       {/* Input field */}
