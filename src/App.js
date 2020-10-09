09d3bb3a50102ce875442007848b076f16170912
Mohammed Faizan Mohiuddin <faizanmohiuddin482@gmail.com>
2020-10-09 20:52:15 +0530

    added message component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
+
 import './App.css';
 
 function App() {
+  //Initialise the state, example of react hook
+  const [input,setInput] = useState('');
+  const [messages,setMessages] = useState(['hello']);
+  console.log(input);
+console.log(messages);
+  const sendMessage = (event)=>{
+    //all the logic to send a message goes here
+    setMessages([...messages,input])
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Messenger</h1>
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <button onClick={sendMessage}>Send Message</button>
+
+          {messages.map(message=>(
+            <p>{message}</p>
+          ))}
+
+      {/* Input field */}
+      {/* Send button */}
+      {/* View all the messages */}
     </div>
   );
 }
